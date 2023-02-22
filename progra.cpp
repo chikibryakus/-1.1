46d1f8ec8b2cb162a22312a981926b858d74e4fd
Mira <dudova_2001@mail.ru>
2023-02-23 02:48:02 +0300

    Add if

--- progra.cpp
+++ progra.cpp
@@ -8,3 +8,29 @@ int main()
 {
     int getch();
     setlocale(LC_ALL, "Russian");
+    int x, y;
+
+    printf("Введите Х:");
+    scanf_s("%d", &x);
+    printf("Введите Y:");
+    scanf_s("%d", &y);
+
+    int i = y;
+    int c = 0;
+    int d = 0;
+
+
+    if (abs(x - y) > 10)
+    {
+        if (x < y)
+        {
+            while (x < i)
+            {
+                d++;
+                printf("Операция%i", d);
+                int c = c + i;
+                i++;
+
+            }
+        }
+        printf("Результат=%i", c);
