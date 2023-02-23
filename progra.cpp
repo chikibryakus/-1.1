da72a85123317c78d1bf68051353fb3ce0206d6d
Mira <dudova_2001@mail.ru>
2023-02-23 03:18:08 +0300

    Add code int main

--- progra.cpp
+++ progra.cpp
@@ -8,3 +8,11 @@ int main()
     int getch();
     setlocale(LC_ALL, "Russian");
     int x, y;
+    printf("Введите Х:");
+    scanf_s("%d", &x);
+    printf("Введите Y:");
+    scanf_s("%d", &y);
+
+    int i = y;
+    int c = 0;
+    int d = 0;
